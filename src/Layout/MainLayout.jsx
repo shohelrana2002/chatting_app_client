b0e4f67551a8cd1f40187805e58ff5076ba29eb7
shohelrana2002 <rana2021tang@gmail.com>
2025-03-07 19:29:41 +0600

    Use axios secure and use coomon Profile add user info send to data base

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import NavBar from "../Shared/NavBar/NavBar";
 import Footer from "../Shared/Footer/Footer";
-// 2
+// 2 3
 const MainLayout = () => {
   return (
     <>
