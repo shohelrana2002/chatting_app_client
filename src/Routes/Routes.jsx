a32159c92e07e65753bb6c8d7a1116201f2537e1
shohelrana2002 <rana2021tang@gmail.com>
2025-03-14 21:04:11 +0600

    Dele Home jsx layout

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router";
-import MainLayout from "../Layout/MainLayout";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
-import Home from "../Home/Home/Home";
-import About from "../Home/About/About";
 import DashboardLayout from "../Layout/DashboardLayout";
 import HomeDashboard from "../Pages/Dashboard/Home/HomeDashboard";
 import Message from "../Pages/Dashboard/Pages/Message";
@@ -11,29 +8,6 @@ import Setting from "../Pages/Dashboard/Pages/Setting";
 import NotificationUser from "../Pages/Dashboard/Pages/NotificationUser";
 
 export const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <MainLayout />, //
-  //   children: [
-  //     {
-  //       index: true,
-  //       element: <Home />,
-  //     },
-
-  //     {
-  //       path: "/about",
-  //       element: <About />,
-  //     },
-  //     {
-  //       path: "/register",
-  //       element: <Register />,
-  //     },
-  //     {
-  //       path: "/login",
-  //       element: <Login />,
-  //     },
-  //   ],
-  // },
   {
     path: "/",
     element: <DashboardLayout />,
