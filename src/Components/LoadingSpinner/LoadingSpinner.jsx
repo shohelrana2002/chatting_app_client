20fbb743b042afc331f77f09b92f36ffeeefcaa4
shohelrana2002 <rana2021tang@gmail.com>
2025-03-15 21:51:03 +0600

    spinner add

--- src/Components/LoadingSpinner/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import ClipLoader from "react-spinners/ClipLoader";
+import CirclesWithBar from "react-spinners/CirclesWithBar";
 const LoadingSpinner = () => {
   return (
     <div className="min-h-screen flex bg-gray-300 justify-center items-center">
-      <ClipLoader
-        size={110}
-        aria-label="Loading Spinner"
-        data-testid="loader"
+      <CirclesWithBar
+        height="100"
+        width="100"
+        color="#4fa94d"
+        outerCircleColor="#4fa94d"
+        innerCircleColor="#4fa94d"
+        barColor="#4fa94d"
+        ariaLabel="circles-with-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
       />
     </div>
   );
