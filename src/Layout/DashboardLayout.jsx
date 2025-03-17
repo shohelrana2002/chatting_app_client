51991955fd98db274230a578e147104f625306ea
shohelrana2002 <rana2021tang@gmail.com>
2025-03-17 20:51:01 +0600

    spinner add

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -10,7 +10,7 @@ const DashboardLayout = () => {
   if (loading) return <LoadingSpinner />;
   // 4 5 6 7 8 9 10
   // 11 12 13 14 15 16 17 18 19 20
-  // 21 22
+  // 21 22 23
   return (
     <div className="max-w-[1400px] mx-auto">
       {user ? (
