dd4b90255cfbbee6cc40338fc1c72c38045e5b2d
shohelrana2002 <rana2021tang@gmail.com>
2025-06-05 23:09:54 +0600

    add to google login save in fierbase dtata real time

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
       // save firebase
       const database = getDatabase();
       // let userRef = push(ref(db, "users/"));
-      // 1 2
+      // 1 2 3
       set(ref(database, `users/${currentUser?.uid}`), {
         username: currentUser?.displayName,
         email: currentUser?.email,
