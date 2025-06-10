fda6dd174cb9bce6ad786fd30bd4d70492718d39
shohelrana2002 <rana2021tang@gmail.com>
2025-06-10 22:28:57 +0600

    User Fierbase and data update and fierbase data base export

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
       }
       // save firebase
       const database = getDatabase();
-      // let userRef = push(ref(db, "users/"));
+      // let userRef = push(ref(db, "users/")); 2
 
       set(ref(database, `users/${currentUser?.uid}`), {
         username: currentUser?.displayName,
